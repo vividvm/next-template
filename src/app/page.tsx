eb010fba553cec7ed8f62c4399bfecedbdc8dedd
vividvm <maneesh.vivid@gmail.com>
2023-05-29 17:13:09 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex w-screen h-screen justify-center items-center">
+      <h1 className="text-white">Welcome to next template</h1>
+    </div>
+  );
+}
